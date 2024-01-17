4695ef09f8932add62bc36875a3f417734454ae5
Caio Molinaro <cmolinaro19@gmail.com>
2024-01-16 22:43:16 -0300

    feat: Created PointEntity

---
+++ src/Api/Features/Point/DataAccess/PointEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace SPW.Admin.Api.Features.Point.DataAccess;
+
+[ExcludeFromCodeCoverage]
+[DynamoDBTable(TableName)]
+internal sealed class PointEntity
+{
+    public const string TableName = "point";
+    public const string HashKeyName = "id";
+
+    [DynamoDBHashKey(HashKeyName)]
+    public Guid Id { get; set; }
+
+    [DynamoDBProperty("name")]
+    public string Name { get; set; }
+
+    [DynamoDBProperty("quantity_publishers")]
+    public int QuantityPublishers { get; set; }
+
+    [DynamoDBProperty("address")]
+    public string Asddress { get; set; }
+}

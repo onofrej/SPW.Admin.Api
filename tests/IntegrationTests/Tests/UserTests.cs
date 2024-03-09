0daab21eb6f9b6dd955bd092efd4e46eba604480
Caio Molinaro <cmolinaro19@gmail.com>
2024-03-09 14:35:41 -0300

    tests: Implemented Bogus Faker to generate random data for tests

--- tests/IntegrationTests/Tests/UserTests.cs
+++ tests/IntegrationTests/Tests/UserTests.cs
@@ -2,6 +2,10 @@
 using SPW.Admin.Api.Features.User.DataAccess;
 using SPW.Admin.Api.Shared.Models;
 using SPW.Admin.IntegrationTests.Fixtures;
+using Bogus;
+using FluentValidation;
+using Serilog;
+using FluentAssertions.Extensions;
 
 namespace SPW.Admin.IntegrationTests.Tests;
 
@@ -11,30 +15,43 @@ public class UserTests : IClassFixture<MainFixture>
     private const string HashKey = "id";
     private readonly Fixture _fixture;
     private readonly MainFixture _mainFixture;
-    private readonly DateTime _birthDate;
-    private readonly DateTime _baptismDate;
+
+    //private readonly DateTime _birthDate;
+    //private readonly DateTime _baptismDate;
 
     public UserTests(MainFixture mainFixture)
     {
         _fixture = new Fixture();
         _mainFixture = mainFixture;
-        _birthDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
-        _baptismDate = new DateTime(DateTime.Now.Year + 10, DateTime.Now.Month, DateTime.Now.Day);
+
+        //_birthDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+        //_baptismDate = new DateTime(DateTime.Now.Year + 10, DateTime.Now.Month, DateTime.Now.Day);
     }
 
     [Fact(DisplayName = "Request received is valid then user is created")]
     public async Task Request_received_is_valid_then_user_is_created()
     {
         //Arrange
-        var request = _fixture.Build<CreateRequest>()
-            .With(propertyPicker => propertyPicker.Email, "johndoe@gmail.com")
-            .With(propertyPicker => propertyPicker.PhoneNumber, "12345678912")
-            .With(propertyPicker => propertyPicker.BirthDate, _birthDate)
-            .With(propertyPicker => propertyPicker.BaptismDate, _baptismDate)
-            .Create();
+        //var request = _fixture.Build<CreateRequest>()
+        //    .With(propertyPicker => propertyPicker.Email, "johndoe@gmail.com")
+        //    .With(propertyPicker => propertyPicker.PhoneNumber, "12345678912")
+        //    .With(propertyPicker => propertyPicker.BirthDate, _birthDate)
+        //    .With(propertyPicker => propertyPicker.BaptismDate, _baptismDate)
+        //    .With(propertyPicker => propertyPicker.BaptismDate, _baptismDate)
+        //    .Create();
+
+        var faker = new Faker<CreateRequest>().StrictMode(true)
+               .RuleFor(x => x.Name, f => f.Name.FullName(Bogus.DataSets.Name.Gender.Male))
+               .RuleFor(x => x.Email, f => f.Internet.Email(f.Person.FirstName.ToLower()))
+               .RuleFor(x => x.PhoneNumber, f => f.Random.ReplaceNumbers("###########"))
+               .RuleFor(x => x.Gender, f => f.PickRandom(new string[] { "male", "female" }))
+               //.RuleFor(x => x.BirthDate, f => f.Date.Recent(3650).ToUniversalTime())
+               //.RuleFor(x => x.BaptismDate, f => f.Date.Recent(350).ToUniversalTime())
+               .RuleFor(x => x.Privilege, f => f.PickRandom(new string[] { "Elder", "Pioneer", "Ministerial Servant" }));
 
         //Act
-        var rawResponse = await _mainFixture.HttpClient.PostAsJsonAsync(RequestUri, request);
+        var entity = faker.Generate();
+        var rawResponse = await _mainFixture.HttpClient.PostAsJsonAsync(RequestUri, entity);
         var response = await rawResponse.Content.ReadFromJsonAsync<Response<Guid>>();
 
         //Assert
@@ -44,7 +61,7 @@ public class UserTests : IClassFixture<MainFixture>
         var userEntity = await _mainFixture.DynamoDbFixture
             .ReadAsync<UserEntity>(HashKey, response!.Data.ToString());
 
-        userEntity.Should().BeEquivalentTo(request);
+        userEntity.Should().BeEquivalentTo(entity);
     }
 
     [Fact(DisplayName = "Request received is invalid then user is not created")]

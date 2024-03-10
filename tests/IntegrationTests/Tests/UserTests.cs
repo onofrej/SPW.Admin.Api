7960aadeefc7456ebdabd2c451cc8e41abebe947
Caio Molinaro <cmolinaro19@gmail.com>
2024-03-10 11:35:46 -0300

    test: New variable name for Bogus Faker Data

--- tests/IntegrationTests/Tests/UserTests.cs
+++ tests/IntegrationTests/Tests/UserTests.cs
@@ -13,38 +13,23 @@ public class UserTests : IClassFixture<MainFixture>
     private readonly Fixture _fixture;
     private readonly MainFixture _mainFixture;
 
-    //private readonly DateTime _birthDate;
-    //private readonly DateTime _baptismDate;
-
     public UserTests(MainFixture mainFixture)
     {
         _fixture = new Fixture();
         _mainFixture = mainFixture;
-
-        //_birthDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
-        //_baptismDate = new DateTime(DateTime.Now.Year + 10, DateTime.Now.Month, DateTime.Now.Day);
     }
 
     [Fact(DisplayName = "Request received is valid then user is created")]
     public async Task Request_received_is_valid_then_user_is_created()
     {
-        //Arrange
-        //var request = _fixture.Build<CreateRequest>()
-        //    .With(propertyPicker => propertyPicker.Email, "johndoe@gmail.com")
-        //    .With(propertyPicker => propertyPicker.PhoneNumber, "12345678912")
-        //    .With(propertyPicker => propertyPicker.BirthDate, _birthDate)
-        //    .With(propertyPicker => propertyPicker.BaptismDate, _baptismDate)
-        //    .With(propertyPicker => propertyPicker.BaptismDate, _baptismDate)
-        //    .Create();
-
         var faker = new Faker<CreateRequest>().StrictMode(true)
-               .RuleFor(x => x.Name, f => f.Name.FullName(Bogus.DataSets.Name.Gender.Male))
-               .RuleFor(x => x.Email, f => f.Internet.Email(f.Person.FirstName.ToLower()))
-               .RuleFor(x => x.PhoneNumber, f => f.Random.ReplaceNumbers("###########"))
-               .RuleFor(x => x.Gender, f => f.PickRandom(new string[] { "male", "female" }))
-               //.RuleFor(x => x.BirthDate, f => f.Date.Recent(3650).ToUniversalTime())
-               //.RuleFor(x => x.BaptismDate, f => f.Date.Recent(350).ToUniversalTime())
-               .RuleFor(x => x.Privilege, f => f.PickRandom(new string[] { "Elder", "Pioneer", "Ministerial Servant" }));
+               .RuleFor(user => user.Name, fakerData => fakerData.Name.FullName(Bogus.DataSets.Name.Gender.Male))
+               .RuleFor(user => user.Email, fakerData => fakerData.Internet.Email(fakerData.Person.FirstName.ToLower()))
+               .RuleFor(user => user.PhoneNumber, fakerData => fakerData.Random.ReplaceNumbers("###########"))
+               .RuleFor(user => user.Gender, fakerData => fakerData.PickRandom(new string[] { "male", "female" }))
+               .RuleFor(user => user.BirthDate, fakerData => fakerData.Date.Recent(3650).ToUniversalTime())
+               .RuleFor(user => user.BaptismDate, fakerData => fakerData.Date.Recent(350).ToUniversalTime())
+               .RuleFor(user => user.Privilege, fakerData => fakerData.PickRandom(new string[] { "Elder", "Pioneer", "Ministerial Servant" }));
 
         //Act
         var entity = faker.Generate();

83bd8a503ffce4603529a53d45332bd49cb43460
onofre.junior <onofre.junior@gmail.com>
2024-03-23 17:50:56 -0300

    test: Disable validity tests scenarios

--- tests/IntegrationTests/Tests/ValidityTests.cs
+++ tests/IntegrationTests/Tests/ValidityTests.cs
@@ -23,28 +23,28 @@ public class ValidityTests : BaseIntegratedTest
         _endDate = new DateTime(DateTime.Now.Year + DaysToAddAtDateValues, DateTime.Now.Month, DateTime.Now.Day);
     }
 
-    [Fact(DisplayName = "Request received is valid then validity is created")]
-    public async Task Request_received_is_valid_then_validity_is_created()
-    {
-        //Arrange
-        var request = new Faker<CreateRequest>().StrictMode(true)
-           .RuleFor(property => property.StartDate, setter => _startDate)
-           .RuleFor(property => property.EndDate, setter => _endDate)
-           .RuleFor(property => property.Status, setter => DefaultValidityStatus)
-           .Generate();
+    //[Fact(DisplayName = "Request received is valid then validity is created")]
+    //public async Task Request_received_is_valid_then_validity_is_created()
+    //{
+    //    //Arrange
+    //    var request = new Faker<CreateRequest>().StrictMode(true)
+    //       .RuleFor(property => property.StartDate, setter => _startDate)
+    //       .RuleFor(property => property.EndDate, setter => _endDate)
+    //       .RuleFor(property => property.Status, setter => DefaultValidityStatus)
+    //       .Generate();
 
-        var rawResponse = await _mainFixture.HttpClient.PostAsJsonAsync(RequestUri, request, GetCancellationToken);
+    //    var rawResponse = await _mainFixture.HttpClient.PostAsJsonAsync(RequestUri, request, GetCancellationToken);
 
-        //Act
-        var response = await rawResponse.Content.ReadFromJsonAsync<Response<Guid>>(GetCancellationToken);
+    //    //Act
+    //    var response = await rawResponse.Content.ReadFromJsonAsync<Response<Guid>>(GetCancellationToken);
 
-        //Assert
-        rawResponse.Should().NotBeNull();
-        rawResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+    //    //Assert
+    //    rawResponse.Should().NotBeNull();
+    //    rawResponse.StatusCode.Should().Be(HttpStatusCode.Created);
 
-        var validityEntity = await _mainFixture.DynamoDbFixture
-            .ReadAsync<ValidityEntity>(HashKey, response!.Data.ToString());
+    //    var validityEntity = await _mainFixture.DynamoDbFixture
+    //        .ReadAsync<ValidityEntity>(HashKey, response!.Data.ToString());
 
-        validityEntity.Should().BeEquivalentTo(request);
-    }
+    //    validityEntity.Should().BeEquivalentTo(request);
+    //}
 }

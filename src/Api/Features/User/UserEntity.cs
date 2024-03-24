0c6bcf2b7ebaf82106b09570b0e461de4885fa4a
onofre.junior <onofre.junior@gmail.com>
2024-03-24 10:21:24 -0300

    feat: Added new logic to handle with database connection

--- src/Api/Features/User/UserEntity.cs
+++ src/Api/Features/User/UserEntity.cs
@@ -3,21 +3,14 @@
 [ExcludeFromCodeCoverage]
 internal sealed class UserEntity
 {
-    public Guid Id { get; set; }
-
-    public string? Name { get; set; }
-
-    public DateTime CreationDate { get; set; }
-
-    public string? Email { get; set; }
-
-    public string? PhoneNumber { get; set; }
-
-    public string? Gender { get; set; }
-
-    public DateTime BirthDate { get; set; }
-
     public DateTime BaptismDate { get; set; }
-
+    public DateTime BirthDate { get; set; }
+    public DateTime CreationDate { get; set; }
+    public Guid CongregationId { get; set; }
+    public Guid Id { get; set; }
+    public string? Email { get; set; }
+    public string? Gender { get; set; }
+    public string? Name { get; set; }
+    public string? PhoneNumber { get; set; }
     public string? Privilege { get; set; }
 }

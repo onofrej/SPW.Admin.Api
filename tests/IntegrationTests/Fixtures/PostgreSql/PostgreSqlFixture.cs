e6eca2b0360974e20612c9d6650286ab3bf03ba1
onofre.junior <onofre.junior@gmail.com>
2024-03-26 11:30:46 -0300

    test: Added integration tests for Domain feature

--- tests/IntegrationTests/Fixtures/PostgreSql/PostgreSqlFixture.cs
+++ tests/IntegrationTests/Fixtures/PostgreSql/PostgreSqlFixture.cs
@@ -1,10 +1,11 @@
-﻿using Npgsql;
+﻿using System.Threading;
 
 namespace SPW.Admin.IntegrationTests.Fixtures.PostgreSql;
 
 internal sealed class PostgreSqlFixture : IDisposable
 {
     private const string ApplicationName = "SPW.Admin.Api";
+    private readonly string _connectionString;
 
     public PostgreSqlFixture(IConfiguration _configuration)
     {
@@ -12,23 +13,46 @@ internal sealed class PostgreSqlFixture : IDisposable
         string filePath = rootPath[..(rootPath.IndexOf(ApplicationName) + ApplicationName.Length)];
         string databaseScriptPath = string.Concat(filePath, "\\infra\\terraform\\scripts\\database-script.sql");
 
-        CreateDatabase(_configuration, databaseScriptPath);
+        _connectionString = _configuration.GetSection("PostgreSQL:ConnectionString").Value!;
+
+        ArgumentException.ThrowIfNullOrEmpty(_connectionString);
+
+        CreateDatabase(databaseScriptPath);
     }
 
-    private static void CreateDatabase(IConfiguration configuration, string databaseScriptPath)
+    private void CreateDatabase(string databaseScriptPath)
     {
-        var connectionString = configuration.GetSection("PostgreSQL:ConnectionString").Value;
         var databaseScript = File.ReadAllText(databaseScriptPath);
 
-        using var connection = new NpgsqlConnection(connectionString);
+        using var connection = new NpgsqlConnection(_connectionString);
         connection.Open();
 
         using var command = new NpgsqlCommand(databaseScript, connection);
         command.ExecuteNonQuery();
     }
 
+    public async Task<T?> GetByIdAsync<T>(string query, Guid id, CancellationToken cancellationToken)
+    {
+        using var connection = new NpgsqlConnection(_connectionString);
+        await connection.OpenAsync(cancellationToken);
+        return await connection.QueryFirstOrDefaultAsync<T?>(query, new { Id = id });
+    }
+
+    public async Task<int> CreateAsync<T>(T entity, string query, CancellationToken cancellationToken)
+    {
+        using var connection = new NpgsqlConnection(_connectionString);
+        await connection.OpenAsync(cancellationToken);
+        return await connection.ExecuteAsync(query, entity);
+    }
+
+    internal async Task TruncateTableAsync(string tableName, CancellationToken cancellationToken)
+    {
+        using var connection = new NpgsqlConnection(_connectionString);
+        await connection.OpenAsync(cancellationToken);
+        await connection.ExecuteAsync($"TRUNCATE TABLE {tableName};");
+    }
+
     public void Dispose()
     {
-        throw new NotImplementedException();
     }
 }
